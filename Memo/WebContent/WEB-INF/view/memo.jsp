f72b5a70275123b7975fe96529c7f37acaeb847e
里見 凱夫 < @192.168.2.122>
2023-10-12 13:26:59 +0900

    [update]メモアプリを２ちゃん風掲示板に変更

--- Memo/WebContent/WEB-INF/view/memo.jsp
+++ Memo/WebContent/WEB-INF/view/memo.jsp
@@ -2,29 +2,25 @@
     pageEncoding="UTF-8"%>
 <%@ page import="java.util.List" %>
 <%@ page import="java.util.ArrayList" %>
+<% ArrayList<String[]> list = (ArrayList<String[]>)request.getAttribute("data"); %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Memo</title>
+<title>7ちゃんねる</title>
 </head>
-<body>
-	<h2>Memo</h2>
-	メモをつける
+<body style="width:800px;margin:auto;">
+	<h2>７ちゃんねる</h2>
 	<form method="post" action="./CreateMemo">
-		<input type="text" name="memo">
-		<input type="submit">
+		<input type="text" name="memo_text">
+		<input type="submit" value="投稿">
 	</form>
-	<h2>Memo</h2>
-	<% List<String> list = new ArrayList<String>(); %>
-	<% list = (List<String>)request.getAttribute("list"); %>
-	<% if (list != null) { %>
-    	<% for (String str : list) { %>
-        	<%= str %><br>
-    	<% } %>
-    <% } else { %>
-    	何もないよ
-    	<% request.setAttribute(name, o) %>>
-    <% } %>
+	<%
+		for (int i = 0; i < list.size(); i++) {
+			String[] data = list.get(i);
+			out.print(data[0] + ". create_at:" + data[2] + "<br>");
+			out.print( data[1] + "<br><br>");
+		}
+	%>
 </body>
 </html>
--- Memo/src/pack/CreateMemo.java
+++ Memo/src/pack/CreateMemo.java
@@ -1,8 +1,13 @@
 package pack;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,7 +22,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CreateMemo")
 public class CreateMemo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	//DBに接続するための情報をフィールドに定数として定義
+	public static final String URL = "jdbc:mysql://localhost/memo";
+	public static final String USER = "study";
+    public static final String PASSWORD = "diamond-f";
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,9 +38,39 @@ public class CreateMemo extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//データを格納するリスト
+		ArrayList<String[]> list = new ArrayList<String[]>();
+		
+		//DBに接続する
+		try {
+			//JDBCドライバの検出
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//DBへのアクセス
+			try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+				//DBへのアクセス
+				Statement st = conn.createStatement();){
+				//SQL文の入力
+				ResultSet res = st.executeQuery("SELECT * FROM memo ORDER BY memo_id DESC");
+				
+				while(res.next()) {
+					String[] data = new String[3];
+					data[0] = res.getString("memo_id");
+					data[1] = res.getString("memo_text");
+					data[2] = res.getString("date");
+					list.add(data);
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		//JSPに送るためのデータを格納
+		request.setAttribute("data", list);
+		
 		String view = "WEB-INF/view/memo.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
-		
 		dispatcher.forward(request, response);
 	}
 
@@ -40,21 +78,39 @@ public class CreateMemo extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// メモを入れる配列を作る
-		List<String> memoList = new ArrayList<String>();
-		// 文字コードを指定
-//		List<String> nextList = (List<String>)request.getAttribute("nextList");
+		//文字化け対策
 		request.setCharacterEncoding("UTF-8");
-		// formから値を取得
-		String memo = request.getParameter("memo");
-		// 取得した値を配列に代入
-		memoList.add(memo);
-		// 配列をJSPに渡す
-		request.setAttribute("list", memoList);
+		response.setContentType("text/html;charset=UTF8");
 		
-//		List<String> secondList = (List<String>) request.getAttribute("nextList");
-//		System.out.println(nextList);
+		//requestオブジェクトから取得
+		String memo = request.getParameter("memo_text");
+		
+		//メモが空文字でなければ処理を続ける
+		if (!memo.equals("")) {
+			//データベース接続情報管理
+			Connection conn = null;
+			
+			//SQL情報管理
+			PreparedStatement ps = null;
+			
+			try {
+				//JDBCドライバのインストール
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				//DB接続
+				conn = (Connection) DriverManager.getConnection(URL, USER, PASSWORD);
+				
+				//SQL文（追加処理）の作成と実行
+				String sql = "INSERT INTO memo(memo_text,date) VALUES(?,now())";
+				ps = conn.prepareStatement(sql);
+				ps.setString(1, memo);
+				ps.executeUpdate();
+			} catch (Exception e) {
+				//何もしない。本来は例外処理を書く
+				//↓エラー文を段階的に表示してくれる
+				e.printStackTrace();
+			}
+		}
 		doGet(request, response);
 	}
-
 }
